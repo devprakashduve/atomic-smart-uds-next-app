bff1ccacdcdcc0407f540d92dd147c8a925cc688
devprakashduve <dev876.1@gmail.com>
2025-01-27 09:16:59 +0530

    atoms component refine - v10

--- src/components/atoms/InputGroup/Switch_delete/SwitchProps.interface.tsx
+++
@@ -1,9 +0,0 @@
-export interface SwitchProps {
-  checked: boolean; // Whether the switch is ON or OFF
-  onChange: (checked: boolean) => void; // Callback when the switch state changes
-  disabled?: boolean; // Optional flag to disable the switch
-  textForOn?: string;
-  textForOff?: string;
-  disableIcons?: boolean;
-  noBackground?: boolean;
-}
